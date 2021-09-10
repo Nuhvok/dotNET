fcdafe9e8c29542b1a400a80927f3c737a2abb48
Nuhvok <51329777+Nuhvok@users.noreply.github.com>
2021-09-10 12:14:14 -0400

    Add files via upload

---
+++ FastFoodOrdering/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿// Brandon Rolfe
+// CS 364
+// Project #4 (FastFoodOrdering)
+// 3/22/21
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FastFoodOrdering
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Order currentOrder = new Order();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initializes labels with data from the Order
+            First_Item.Content  = ((Sandwich.SandwichTypeList)0) + " Sandwich";
+            Second_Item.Content = ((Sandwich.SandwichTypeList)1) + " Sandwich";
+            Third_Item.Content  = ((Sandwich.SandwichTypeList)2) + " Sandwich";
+            Fourth_Item.Content = ((Sandwich.SandwichTypeList)3) + " Sandwich";
+            Fifth_Item.Content  = "Large Fries";
+            Sixth_Item.Content  = "Large Coke";
+
+            Tax_Label.Content = "Tax(" + Order.TAX_RATE + "):";
+
+            First_Price.Content  = Order.BEEF_PRICE.ToString("C2");
+            Second_Price.Content = Order.PORK_PRICE.ToString("C2");
+            Third_Price.Content  = Order.CHICKEN_PRICE.ToString("C2");
+            Fourth_Price.Content = Order.SAUSAGE_PRICE.ToString("C2");
+            Fifth_Price.Content  = Order.FRIES_PRICE.ToString("C2");
+            Sixth_Price.Content  = Order.DRINK_PRICE.ToString("C2");
+
+            First_Sandwich_Choice.Content  = (Sandwich.SandwichTypeList)0;
+            Second_Sandwich_Choice.Content = (Sandwich.SandwichTypeList)1;
+            Third_Sandwich_Choice.Content  = (Sandwich.SandwichTypeList)2;
+            Fourth_Sandwich_Choice.Content = (Sandwich.SandwichTypeList)3;
+
+            First_Topping_Choice.Content  = (Sandwich.ToppingsList)0;
+            Second_Topping_Choice.Content = (Sandwich.ToppingsList)1;
+            Third_Topping_Choice.Content  = (Sandwich.ToppingsList)2;
+            Fourth_Topping_Choice.Content = (Sandwich.ToppingsList)3;
+            Fifth_Topping_Choice.Content  = (Sandwich.ToppingsList)4;
+            Sixth_Topping_Choice.Content  = (Sandwich.ToppingsList)5;
+
+            UpdateOutputs();
+        }
+
+        // Updates the order and cost fields
+        public void UpdateOutputs()
+        {
+            Order_List.Content = currentOrder.ToString();
+            Subtotal.Content   = currentOrder.GetSubtotal().ToString("C2");
+            Tax.Content        = currentOrder.GetTax().ToString("C2");
+            Total.Content      = currentOrder.GetTotal().ToString("C2");
+            Status.Content = "All Good";
+        }
+
+        // Adds a sandwich to the Order when clicked
+        private void Add_Sandwich_Click(object sender, RoutedEventArgs e)
+        {
+            int sandwichType;
+            try
+            {
+                // Checks which sandwich type was chosen
+                if (First_Sandwich_Choice.IsChecked.Value)
+                {
+                    sandwichType = 0;
+                    First_Sandwich_Choice.IsChecked = false;
+                }
+                else if (Second_Sandwich_Choice.IsChecked.Value)
+                {
+                    sandwichType = 1;
+                    Second_Sandwich_Choice.IsChecked = false;
+                }
+                else if (Third_Sandwich_Choice.IsChecked.Value)
+                {
+                    sandwichType = 2;
+                    Third_Sandwich_Choice.IsChecked = false;
+                }
+                else if (Fourth_Sandwich_Choice.IsChecked.Value)
+                {
+                    sandwichType = 3;
+                    Fourth_Sandwich_Choice.IsChecked = false;
+                }
+                else
+                    throw new ArgumentException("Add_Sandwich_Click", "You must choose a type of sandwich to add.");
+            }
+            catch(ArgumentException ex)
+            {
+                Status.Content = "You must choose a type of sandwich to add.";
+                return;
+            }
+            // Counts how many toppings were chosen
+            int numOfToppings = 0;
+            if (First_Topping_Choice.IsChecked.Value)
+                numOfToppings++;
+            if (Second_Topping_Choice.IsChecked.Value)
+                numOfToppings++;
+            if (Third_Topping_Choice.IsChecked.Value)
+                numOfToppings++;
+            if (Fourth_Topping_Choice.IsChecked.Value)
+                numOfToppings++;
+            if (Fifth_Topping_Choice.IsChecked.Value)
+                numOfToppings++;
+            if (Sixth_Topping_Choice.IsChecked.Value)
+                numOfToppings++;
+            
+            // Creates topping list and adds the chosen toppings to it
+            int[] toppingsList = new int[numOfToppings];
+            int nextIndex = 0;
+            if (First_Topping_Choice.IsChecked.Value)
+            {
+                toppingsList[nextIndex] = 0;
+                First_Topping_Choice.IsChecked = false;
+                nextIndex++;
+            }
+            if (Second_Topping_Choice.IsChecked.Value)
+            {
+                toppingsList[nextIndex] = 1;
+                Second_Topping_Choice.IsChecked = false;
+                nextIndex++;
+            }
+            if (Third_Topping_Choice.IsChecked.Value)
+            {
+                toppingsList[nextIndex] = 2;
+                Third_Topping_Choice.IsChecked = false;
+                nextIndex++;
+            }
+            if (Fourth_Topping_Choice.IsChecked.Value)
+            {
+                toppingsList[nextIndex] = 3;
+                Fourth_Topping_Choice.IsChecked = false;
+                nextIndex++;
+            }
+            if (Fifth_Topping_Choice.IsChecked.Value)
+            {
+                toppingsList[nextIndex] = 4;
+                Fifth_Topping_Choice.IsChecked = false;
+                nextIndex++;
+            }
+            if (Sixth_Topping_Choice.IsChecked.Value)
+            {
+                toppingsList[nextIndex] = 5;
+                Sixth_Topping_Choice.IsChecked = false;
+                nextIndex++;
+            }
+
+            currentOrder.AddSandwich(sandwichType, toppingsList);
+            UpdateOutputs();
+        }
+
+        // Adds one order of fries to the Order on click
+        private void Add_Fries_Click(object sender, RoutedEventArgs e)
+        {
+            currentOrder.AddFries();
+            UpdateOutputs();
+        }
+
+        // Adds one drink to the Order on click
+        private void Add_Drink_Click(object sender, RoutedEventArgs e)
+        {
+            currentOrder.AddDrink();
+            UpdateOutputs();
+        }
+    }
+}
---
+++ FastFoodOrdering/Order.cs
@@ -0,0 +1,175 @@
+﻿// Brandon Rolfe
+// CS 364
+// Project #4 (FastFoodOrdering)
+// 3/22/21
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FastFoodOrdering
+{
+    class Order
+    {
+        public const float BEEF_PRICE = 4.99f,
+                           PORK_PRICE = 5.49f,
+                           CHICKEN_PRICE = 3.99f,
+                           SAUSAGE_PRICE = 4.24f,
+                           FRIES_PRICE = 2.49F,
+                           DRINK_PRICE = 1.99F,
+                           TAX_RATE      = 0.07f;
+
+        // Fields for Order
+        Sandwich[] sandwichOrder;
+        int fries;
+        int drinks;
+
+        public Order()
+        {
+            SetSandwiches(new Sandwich[0]);
+            SetFries(0);
+            SetDrinks(0);
+        }
+
+        public Order(Sandwich[] newSandwiches, int newFries, int newDrinks)
+        {
+            SetSandwiches(newSandwiches);
+            SetFries(newFries);
+            SetDrinks(newDrinks);
+        }
+
+        // Gets and sets the Order fields
+        public Sandwich[] GetSandwiches() => sandwichOrder;
+        public void SetSandwiches(Sandwich[] newSandwiches) => sandwichOrder = newSandwiches;
+        public int GetFries() => fries;
+        public void SetFries(int newFries)
+        {
+            if (newFries < 0)
+                throw new ArgumentOutOfRangeException("SetFries", "Amount of fries cannot be below 0");
+            else
+                fries = newFries;
+        }
+        public int GetDrinks() => drinks;
+        public void SetDrinks(int newDrinks)
+        {
+            if (newDrinks < 0)
+                throw new ArgumentOutOfRangeException("SetDrinks", "Amount of drinks cannot be below 0");
+            else
+                drinks = newDrinks;
+        }
+
+        // Adds a snadwich to the sandwich list
+        public void AddSandwich(int SandwichType, int[] Toppings)
+        {
+            Sandwich[] tempSandwichOrder = sandwichOrder;
+            sandwichOrder = new Sandwich[tempSandwichOrder.Length + 1];
+            for(int i = 0; i < tempSandwichOrder.Length; i++)
+            {
+                sandwichOrder[i] = tempSandwichOrder[i];
+            }
+            sandwichOrder[sandwichOrder.Length - 1] = new Sandwich(SandwichType, Toppings);
+        }
+
+        // Adds fries or a drink to the Order
+        public void AddFries() => fries++;
+        public void AddDrink() => drinks++;
+
+        // Gets the subtotal of the Order
+        public float GetSubtotal()
+        {
+            float subtotal = 0.0f;
+            foreach (Sandwich item in sandwichOrder)
+            {
+                subtotal += item.GetPrice();
+            }
+
+            subtotal += fries * FRIES_PRICE;
+            subtotal += drinks * DRINK_PRICE;
+
+            return subtotal;
+        }
+
+        // Gets the tax of the Order
+        public float GetTax() => GetSubtotal() * TAX_RATE;
+
+        // Gets the total of the Order
+        public float GetTotal() => GetSubtotal() + GetTax();
+        
+        // Gets the order as a string
+        public override string ToString()
+        {
+            string outputString = "Current Order:\n";
+            // Adds the counts to the Order string
+            outputString += "Sandwiches: " + sandwichOrder.Length + "\n";
+            outputString += "Fries: " + fries + "\n";
+            outputString += "Drinks: " + drinks + "\n";
+
+            // Loops for each sandwich, adding it to the Order string
+            foreach (Sandwich item in sandwichOrder)
+            {
+                outputString += "Sandwich Type: " + (Sandwich.SandwichTypeList)item.GetSandwichType() + "\n";
+                outputString += "Toppings: ";
+                // Loops for each topping, adding it to the Order string
+                foreach (int topping in item.GetToppings())
+                {
+                    outputString += (Sandwich.ToppingsList)topping + " ";
+                }
+                outputString += "\n";
+            }
+            return outputString;
+        }
+    }
+
+    struct Sandwich
+    {
+        // Fields of a Sandwich
+        int sandwichType;
+        int[] toppings;
+        float price;
+
+        public Sandwich(int SandwichType, int[] Toppings)
+        {
+            this.sandwichType = SandwichType;
+            this.toppings = Toppings;
+            price = sandwichType switch
+            {
+                0 => Order.BEEF_PRICE,
+                1 => Order.PORK_PRICE,
+                2 => Order.CHICKEN_PRICE,
+                _ => Order.SAUSAGE_PRICE,
+            };
+        }
+
+        // Gets and sets the Sandwich fields
+        public int GetSandwichType() => sandwichType;
+        public void SetSandwichType(int newType) => sandwichType = newType;
+        public int[] GetToppings() => toppings;
+        public void SetToppings(int[] newToppings) => toppings = newToppings;
+        public float GetPrice() => price;
+        public void SetPrice(float newPrice)
+        {
+            if (newPrice < 0)
+                throw new ArgumentOutOfRangeException("SetPrice", "Price cannot be below 0");
+            else
+                price = newPrice;
+        }
+
+        public enum SandwichTypeList
+        {
+            Beef,
+            Pork,
+            Chicken,
+            Sausage
+        }
+        
+        public enum ToppingsList
+        {
+            Cheese,
+            Lettuce,
+            Tomato,
+            Ketchup,
+            Mustard,
+            Mayo
+        }
+    }
+}
